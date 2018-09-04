9d232f76539cfbee5514227ae68ddec95b1e8bea
Jessica Dionisio <dionisiojessica15@gmail.com>
2018-09-04 08:43:31 +0800

    Album Photo Listings

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -5,4 +5,42 @@
   <a class="button secondary" href="/">Go Back</a>
   <a class="button" href="/photoshow/public/photos/create/{{$album->id}}">Upload Photo to Album</a>
   <hr>
+
+  @if(count($album->photos) > 0)
+    <?php
+      $colcount = count($album->photos);
+      $i = 1;
+    ?>
+    <div id="albums">
+      <div class="row text-center">
+        @foreach($album->photos as $photo)
+          @if($i == $colcount)
+             <div class='medium-4 columns end'>
+               <a href="/photoshow/public/photos/{{$photo->id}}">
+                  <img class="thumbnail" src="/photoshow/public/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}" style ="height:250px;
+    width:250px;">
+                </a>
+               <br>
+               <h4>{{$photo->title}}</h4>
+          @else
+            <div class='medium-4 columns'>
+              <a href="/photoshow/public/photos/{{$photo->id}}">
+                 <img class="thumbnail" src="/photoshow/public/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}" style ="height:250px;
+   width:250px;">
+               </a>
+              <br>
+              <h4>{{$photo->title}}</h4>
+          @endif
+          @if($i % 3 == 0)
+          </div></div><div class="row text-center">
+          @else
+            </div>
+          @endif
+          <?php $i++; ?>
+        @endforeach
+      </div>
+    </div>
+  @else
+    <p>No Albums To Display</p>
+  @endif
 @endsection
